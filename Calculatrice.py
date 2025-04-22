6517e8628eb490c7b00bfeb8571ff8a541243314
Timothée <timotheeithier@gmail.com>
2025-04-22 17:29:46 +0200

    Mise à jour de la calculatrice, gestion des touches

--- Calculatrice.py
+++ Calculatrice.py
@@ -6,6 +6,34 @@ import math
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
+Max_chiffre= 9
+pile_ecran = []
+
+def touche_appuyee(touche):
+    print('Touche:', touche)
+    if touche in['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
+        traite_touche_chiffre(touche)
+
+def update_screen(value = False):
+    if value:
+        screen.configure(text=value[0:9])
+    else:
+        screen.configure(text='0')
+
+def _get_nombre_as_text_from_pile():
+    text = ''
+    for num in pile_ecran:
+        text += str(num)
+    return text
+
+
+def traite_touche_chiffre(num):
+    if len(pile_ecran) < Max_chiffre:
+        if num == '.' and '.' in pile_ecran:
+            return
+        pile_ecran.append(num)
+        ecran = ''.join(pile_ecran)
+        update_screen(_get_nombre_as_text_from_pile())
 
 app = customtkinter.CTk()
 app.title("Calculatrice")
@@ -16,23 +44,23 @@ screen.grid(row =0, column=0, sticky="E", padx= (10,10), columnspan = 4)
 
 font_button = ("Bauhaus93", 20)
 
-btn_0 = customtkinter.CTkButton(app, text="0", width = 75, height = 75, font=font_button)
-btn_1 = customtkinter.CTkButton(app, text="1", width = 75, height = 75, font=font_button)
-btn_2 = customtkinter.CTkButton(app, text="2", width = 75, height = 75, font=font_button)
-btn_3 = customtkinter.CTkButton(app, text="3", width = 75, height = 75, font=font_button)
-btn_4 = customtkinter.CTkButton(app, text="4", width = 75, height = 75, font=font_button)
-btn_5 = customtkinter.CTkButton(app, text="5", width = 75, height = 75, font=font_button)
-btn_6 = customtkinter.CTkButton(app, text="6", width = 75, height = 75, font=font_button)
-btn_7 = customtkinter.CTkButton(app, text="7", width = 75, height = 75, font=font_button)
-btn_8 = customtkinter.CTkButton(app, text="8", width = 75, height = 75, font=font_button)
-btn_9 = customtkinter.CTkButton(app, text="9", width = 75, height = 75, font=font_button)
-btn_plus = customtkinter.CTkButton(app, text ="+", width = 75, height = 180, font=font_button)
-btn_moin = customtkinter.CTkButton(app, text ="-", width = 75, height = 75, font=font_button)
-btn_fois = customtkinter.CTkButton(app, text ="x", width = 75, height = 75, font=font_button)
-btn_diviser = customtkinter.CTkButton(app, text ="÷", width = 75, height = 75, font=font_button)
-btn_point = customtkinter.CTkButton(app, text =".", width = 75, height = 75, font=font_button)
-btn_Egale = customtkinter.CTkButton(app, text ="=", width = 75, height = 75, font=font_button)
-btn_reset = customtkinter.CTkButton(app, text ="CE", width = 75, height = 75, font=font_button)
+btn_0 = customtkinter.CTkButton(app, text="0", width = 75, height = 75, font=font_button, command=lambda : touche_appuyee('0'))
+btn_1 = customtkinter.CTkButton(app, text="1", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('1'))
+btn_2 = customtkinter.CTkButton(app, text="2", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('2'))
+btn_3 = customtkinter.CTkButton(app, text="3", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('3'))
+btn_4 = customtkinter.CTkButton(app, text="4", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('4'))
+btn_5 = customtkinter.CTkButton(app, text="5", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('5'))
+btn_6 = customtkinter.CTkButton(app, text="6", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('6'))
+btn_7 = customtkinter.CTkButton(app, text="7", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('7'))
+btn_8 = customtkinter.CTkButton(app, text="8", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('8'))
+btn_9 = customtkinter.CTkButton(app, text="9", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('9'))
+btn_plus = customtkinter.CTkButton(app, text ="+", width = 75, height = 180, font=font_button, command =lambda : touche_appuyee('PLUS'))
+btn_moin = customtkinter.CTkButton(app, text ="-", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('MOIN'))
+btn_fois = customtkinter.CTkButton(app, text ="x", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('FOIS'))
+btn_diviser = customtkinter.CTkButton(app, text ="÷", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('DIVISER'))
+btn_point = customtkinter.CTkButton(app, text =".", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('.'))
+btn_Egale = customtkinter.CTkButton(app, text ="=", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('EGALE'))
+btn_reset = customtkinter.CTkButton(app, text ="CE", width = 75, height = 75, font=font_button, command =lambda : touche_appuyee('RESET'))
 
 btn_0.grid(row=6, column=0, padx=(10, 10), pady=(10, 10))
 btn_1.grid(row=5, column=0, padx=(10, 10), pady=(10, 10))
